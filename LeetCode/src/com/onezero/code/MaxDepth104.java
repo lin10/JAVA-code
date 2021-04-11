c7a438955c9b64278d3be4f6cfc73d529254c0b2
one_zero <354701412@qq.com>
2021-04-11 17:20:54 +0800

    【UPDATE】104. 二叉树的最大深度，新增广度优先遍历方法

--- LeetCode/src/com/onezero/code/MaxDepth104.java
+++ LeetCode/src/com/onezero/code/MaxDepth104.java
@@ -2,6 +2,9 @@ package com.onezero.code;
 
 import com.onezero.objects.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaxDepth104 {
 
     /**
@@ -12,13 +15,36 @@ public class MaxDepth104 {
      * 说明: 叶子节点是指没有子节点的节点。
      * 思路：递归
      */
-
+    //递归
     public int maxDepth(TreeNode root) {
         if(root==null) return 0;
         int lCount=0,rCount=0;
         lCount=maxDepth(root.left);
         rCount=maxDepth(root.right);
         return (lCount>rCount?lCount:rCount)+1;
+    }
+
+    //BFD广度优先遍历（层序）
+    public int maxDepth0(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int count=0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size>0){
+                TreeNode treeNode = queue.poll();
+                if(treeNode.left!=null){
+                    queue.offer(treeNode.left);
+                }
+                if(treeNode.right!=null){
+                    queue.offer(treeNode.right);
+                }
+                size--;
+            }
+            count++;
+        }
+        return count;
     }
 
     public int maxDepth1(TreeNode root) {
